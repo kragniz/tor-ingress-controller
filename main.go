048bfc52b12b42a65b7b807b92d022239441446b
Louis Taylor <louis@kragniz.eu>
2018-01-21 02:19:36 +0000

    Generate some config

--- main.go
+++ main.go
@@ -34,12 +34,16 @@ import (
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/workqueue"
+
+	"github.com/kragniz/tor-ingress-controller/tor"
 )
 
 type TorController struct {
 	indexer  cache.Indexer
 	queue    workqueue.RateLimitingInterface
 	informer cache.Controller
+
+	torCfg tor.TorConfiguration
 }
 
 func NewTorController(queue workqueue.RateLimitingInterface, indexer cache.Indexer, informer cache.Controller) *TorController {
@@ -47,30 +51,31 @@ func NewTorController(queue workqueue.RateLimitingInterface, indexer cache.Index
 		informer: informer,
 		indexer:  indexer,
 		queue:    queue,
+		torCfg:   tor.NewTorConfiguration(),
 	}
 }
 
-func (c *TorController) processNextItem() bool {
+func (t *TorController) processNextItem() bool {
 	// Wait until there is a new item in the working queue
-	key, quit := c.queue.Get()
+	key, quit := t.queue.Get()
 	if quit {
 		return false
 	}
 	// Tell the queue that we are done with processing this key. This unblocks the key for other workers
 	// This allows safe parallel processing because two ingresses with the same key are never processed in
 	// parallel.
-	defer c.queue.Done(key)
+	defer t.queue.Done(key)
 
 	// Invoke the method containing the business logic
-	err := c.syncTor(key.(string))
+	err := t.syncTor(key.(string))
 	// Handle the error if something went wrong during the execution of the business logic
-	c.handleErr(err, key)
+	t.handleErr(err, key)
 	return true
 }
 
 // syncTor updates the tor config with the current set of ingresses
-func (c *TorController) syncTor(key string) error {
-	obj, exists, err := c.indexer.GetByKey(key)
+func (t *TorController) syncTor(key string) error {
+	obj, exists, err := t.indexer.GetByKey(key)
 	if err != nil {
 		glog.Errorf("Fetching object with key %s from store failed with %v", key, err)
 		return err
@@ -78,68 +83,77 @@ func (c *TorController) syncTor(key string) error {
 
 	if !exists {
 		fmt.Printf("Ingress %s does not exist anymore\n", key)
+		t.torCfg.RemoveService(key)
+		fmt.Println(t.torCfg.GetConfiguration())
 	} else {
 		switch o := obj.(type) {
 		case *v1beta1.Ingress:
 			// Note that you also have to check the uid if you have a local controlled resource, which
 			// is dependent on the actual instance, to detect that a Ingress was recreated with the same name
 			fmt.Printf("Sync/Add/Update for Ingress %s, namespace %s\n", o.GetName(), o.GetNamespace())
+			backend := o.Spec.Backend
+			if backend == nil {
+				fmt.Println("sorry, only basic backend supported")
+			} else {
+				t.torCfg.AddService(o.GetName(), backend.ServiceName, o.GetNamespace(), int(backend.ServicePort.IntVal), 80)
+				fmt.Println(t.torCfg.GetConfiguration())
+			}
 		}
 	}
 	return nil
 }
 
 // handleErr checks if an error happened and makes sure we will retry later.
-func (c *TorController) handleErr(err error, key interface{}) {
+func (t *TorController) handleErr(err error, key interface{}) {
 	if err == nil {
 		// Forget about the #AddRateLimited history of the key on every successful synchronization.
 		// This ensures that future processing of updates for this key is not delayed because of
 		// an outdated error history.
-		c.queue.Forget(key)
+		t.queue.Forget(key)
 		return
 	}
 
 	// This controller retries 5 times if something goes wrong. After that, it stops trying.
-	if c.queue.NumRequeues(key) < 5 {
+	if t.queue.NumRequeues(key) < 5 {
 		glog.Infof("Error syncing ingress %v: %v", key, err)
 
 		// Re-enqueue the key rate limited. Based on the rate limiter on the
 		// queue and the re-enqueue history, the key will be processed later again.
-		c.queue.AddRateLimited(key)
+		t.queue.AddRateLimited(key)
 		return
 	}
 
-	c.queue.Forget(key)
+	t.queue.Forget(key)
 	// Report to an external entity that, even after several retries, we could not successfully process this key
 	runtime.HandleError(err)
 	glog.Infof("Dropping ingress %q out of the queue: %v", key, err)
 }
 
-func (c *TorController) Run(threadiness int, stopCh chan struct{}) {
+func (t *TorController) Run(threadiness int, stopCh chan struct{}) {
 	defer runtime.HandleCrash()
 
 	// Let the workers stop when we are done
-	defer c.queue.ShutDown()
+	defer t.queue.ShutDown()
 	glog.Info("Starting tor controller")
 
-	go c.informer.Run(stopCh)
+	go t.informer.Run(stopCh)
 
 	// Wait for all involved caches to be synced, before processing items from the queue is started
-	if !cache.WaitForCacheSync(stopCh, c.informer.HasSynced) {
+	if !cache.WaitForCacheSync(stopCh, t.informer.HasSynced) {
 		runtime.HandleError(fmt.Errorf("Timed out waiting for caches to sync"))
 		return
 	}
 
 	for i := 0; i < threadiness; i++ {
-		go wait.Until(c.runWorker, time.Second, stopCh)
+		go wait.Until(t.runWorker, time.Second, stopCh)
 	}
 
 	<-stopCh
 	glog.Info("Stopping tor controller")
 }
 
-func (c *TorController) runWorker() {
-	for c.processNextItem() {
+func (t *TorController) runWorker() {
+	for t.processNextItem() {
 	}
 }
 
--- tor/tor.go
+++ tor/tor.go
@@ -1,11 +1,21 @@
 package tor
 
 import (
+	"bytes"
 	"fmt"
+	"text/template"
 )
 
+const configFormat = `
+{{ range .HiddenServices }}
+HiddenServiceDir /run/tor/{{ .ServiceNamespace}}_{{ .ServiceName }}/
+HiddenServicePort {{ .PublicPort }} {{ .ServiceName }}.{{ .ServiceNamespace}}:{{ .ServicePort }}
+{{ end }}
+`
+
+var configTemplate = template.Must(template.New("config").Parse(configFormat))
+
 type HiddenService struct {
-	Name             string
 	ServiceName      string
 	ServiceNamespace string
 	ServicePort      int
@@ -13,34 +23,31 @@ type HiddenService struct {
 }
 
 type TorConfiguration struct {
-	HiddenServices []HiddenService
+	HiddenServices map[string]HiddenService
 }
 
 func NewTorConfiguration() TorConfiguration {
 	return TorConfiguration{
-		HiddenServices: []HiddenService{},
+		HiddenServices: make(map[string]HiddenService),
 	}
 }
 
-func (t *TorConfiguration) AddService(name, namespace string, servicePort, publicPort int) {
+func (t *TorConfiguration) AddService(name, serviceName, namespace string, servicePort, publicPort int) {
 	s := HiddenService{
-		Name:             fmt.Sprint("%s/%s", namespace, name),
-		ServiceName:      name,
+		ServiceName:      serviceName,
 		ServiceNamespace: namespace,
 		ServicePort:      servicePort,
 		PublicPort:       publicPort,
 	}
-	t.HiddenServices = append(t.HiddenServices, s)
+	t.HiddenServices[fmt.Sprintf("%s/%s", namespace, name)] = s
 }
 
 func (t *TorConfiguration) RemoveService(name string) {
-	var s []HiddenService
-	for _, service := range t.HiddenServices {
-		if service.Name != name {
-			s = append(s, service)
-		}
-	}
+	delete(t.HiddenServices, name)
 }
 
-func (t *TorConfiguration) GetConfiguration() {
+func (t *TorConfiguration) GetConfiguration() string {
+	var tmp bytes.Buffer
+	configTemplate.Execute(&tmp, t)
+	return tmp.String()
 }
